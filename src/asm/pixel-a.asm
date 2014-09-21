375bb87b2fabcca0bf69076038db93cd83225e8b
cantabile <cantabile.desu@gmail.com>
2014-09-21 20:42:38 +0300

    Workaround for incomprehensible linker error in win32

--- src/asm/pixel-a.asm
+++ src/asm/pixel-a.asm
@@ -4191,10 +4191,12 @@ cglobal pixel_hadamard_ac_%1x%2, 2,4,11
 
 ; instantiate satds
 
+%if 0
 %if ARCH_X86_64 == 0
 cextern pixel_sa8d_8x8_internal_mmx2
 INIT_MMX mmx2
 SA8D
+%endif
 %endif
 
 %define TRANS TRANS_SSE2
