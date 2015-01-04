d36ccd4eaf0e294f98371adc25a79eae1c240fd3
cantabile <cantabile.desu@gmail.com>
2015-01-04 19:26:10 +0200

    Sort list of copy functions

--- src/CopyCode.h
+++ src/CopyCode.h
@@ -19,23 +19,23 @@ void Copy_C(uint8_t *pDst, intptr_t nDstPitch, const uint8_t *pSrc, intptr_t nSr
 }
 
 #define MK_CFUNC(functionname) extern "C" void  functionname (uint8_t *pDst, intptr_t nDstPitch, const uint8_t *pSrc, intptr_t nSrcPitch)
-MK_CFUNC(mvtools_Copy32x32_sse2);
+MK_CFUNC(mvtools_Copy2x1_sse2);
+MK_CFUNC(mvtools_Copy2x2_sse2);
+MK_CFUNC(mvtools_Copy2x4_sse2);
+MK_CFUNC(mvtools_Copy4x2_sse2);
+MK_CFUNC(mvtools_Copy4x4_sse2);
+MK_CFUNC(mvtools_Copy4x8_sse2);
+MK_CFUNC(mvtools_Copy8x1_sse2);
+MK_CFUNC(mvtools_Copy8x2_sse2);
+MK_CFUNC(mvtools_Copy8x4_sse2);
+MK_CFUNC(mvtools_Copy8x8_sse2);
+MK_CFUNC(mvtools_Copy8x16_sse2);
+MK_CFUNC(mvtools_Copy16x2_sse2);
+MK_CFUNC(mvtools_Copy16x8_sse2);
+MK_CFUNC(mvtools_Copy16x16_sse2);
 MK_CFUNC(mvtools_Copy16x32_sse2);
 MK_CFUNC(mvtools_Copy32x16_sse2);
-MK_CFUNC(mvtools_Copy16x16_sse2);
-MK_CFUNC(mvtools_Copy16x8_sse2);
-MK_CFUNC(mvtools_Copy16x2_sse2);
-MK_CFUNC(mvtools_Copy8x16_sse2);
-MK_CFUNC(mvtools_Copy8x8_sse2);
-MK_CFUNC(mvtools_Copy8x4_sse2);
-MK_CFUNC(mvtools_Copy8x2_sse2);
-MK_CFUNC(mvtools_Copy8x1_sse2);
-MK_CFUNC(mvtools_Copy4x8_sse2);
-MK_CFUNC(mvtools_Copy4x4_sse2);
-MK_CFUNC(mvtools_Copy4x2_sse2);
-MK_CFUNC(mvtools_Copy2x4_sse2);
-MK_CFUNC(mvtools_Copy2x2_sse2);
-MK_CFUNC(mvtools_Copy2x1_sse2);
+MK_CFUNC(mvtools_Copy32x32_sse2);
 #undef MK_CFUNC
 
 #endif
