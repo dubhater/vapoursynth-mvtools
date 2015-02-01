158b10670ae2dc24415ac5705264534fa93e0d19
cantabile <cantabile.desu@gmail.com>
2015-02-01 20:45:13 +0200

    Clean up the SAD ASM function declarations

--- src/SADFunctions.h
+++ src/SADFunctions.h
@@ -217,65 +217,52 @@ MK_CFUNC(mvtools_sad_32x8_u16_sse2);
 MK_CFUNC(mvtools_sad_32x16_u16_sse2);
 MK_CFUNC(mvtools_sad_32x32_u16_sse2);
 
-/* included from x264 */
-#define SAD_x264(blsizex, blsizey) extern "C" unsigned int  mvtools_pixel_sad_##blsizex##x##blsizey##_mmx2(const uint8_t *pSrc, intptr_t nSrcPitch, const uint8_t *pRef, intptr_t nRefPitch)
-//mvtools_pixel_sad_16x16_mmx2(   x,y can be: 16 8 4
-SAD_x264(16,16);
-SAD_x264(16,8);
-SAD_x264(8,16);
-SAD_x264(8,8);
-SAD_x264(8,4);
-SAD_x264(4,8);
-SAD_x264(4,4);
-#undef SAD_x264
-//parameter is function name
-MK_CFUNC(mvtools_pixel_sad_8x16_sse2);
-MK_CFUNC(mvtools_pixel_sad_16x16_sse2); //non optimized cache access, for AMD?
-MK_CFUNC(mvtools_pixel_sad_16x8_sse2);     //non optimized cache access, for AMD?
-MK_CFUNC(mvtools_pixel_sad_16x16_sse3); //LDDQU Pentium4E (Core1?), not for Core2!
-MK_CFUNC(mvtools_pixel_sad_16x8_sse3);  //LDDQU Pentium4E (Core1?), not for Core2!
-MK_CFUNC(mvtools_pixel_sad_16x16_cache64_ssse3);//core2 optimized
-MK_CFUNC(mvtools_pixel_sad_16x8_cache64_ssse3); //core2 optimized
+// From sad-a.asm - stolen from x264
+MK_CFUNC(mvtools_pixel_sad_4x4_mmx2);
+MK_CFUNC(mvtools_pixel_sad_4x8_mmx2);
+MK_CFUNC(mvtools_pixel_sad_8x4_mmx2);
+MK_CFUNC(mvtools_pixel_sad_8x8_mmx2);
+MK_CFUNC(mvtools_pixel_sad_8x16_mmx2);
+MK_CFUNC(mvtools_pixel_sad_16x8_mmx2);
+MK_CFUNC(mvtools_pixel_sad_16x16_mmx2);
 
-MK_CFUNC(mvtools_pixel_sad_8x16_cache64_mmx2);
-MK_CFUNC(mvtools_pixel_sad_8x8_cache64_mmx2);
 MK_CFUNC(mvtools_pixel_sad_8x4_cache64_mmx2);
+MK_CFUNC(mvtools_pixel_sad_8x8_cache64_mmx2);
+MK_CFUNC(mvtools_pixel_sad_8x16_cache64_mmx2);
 
-//1.9.5.3: added ssd & SATD (TSchniede)
+MK_CFUNC(mvtools_pixel_sad_8x16_sse2);
+MK_CFUNC(mvtools_pixel_sad_16x8_sse2);     //non optimized cache access, for AMD?
+MK_CFUNC(mvtools_pixel_sad_16x16_sse2); //non optimized cache access, for AMD?
+
+MK_CFUNC(mvtools_pixel_sad_16x8_sse3);  //LDDQU Pentium4E (Core1?), not for Core2!
+MK_CFUNC(mvtools_pixel_sad_16x16_sse3); //LDDQU Pentium4E (Core1?), not for Core2!
+
+MK_CFUNC(mvtools_pixel_sad_16x8_cache64_ssse3); //core2 optimized
+MK_CFUNC(mvtools_pixel_sad_16x16_cache64_ssse3);//core2 optimized
+
+// From pixel-a.asm - stolen from x264
 /* alternative to SAD - SSD: squared sum of differences, VERY sensitive to noise */
-MK_CFUNC(mvtools_pixel_ssd_16x16_mmx);
-MK_CFUNC(mvtools_pixel_ssd_16x8_mmx);
-MK_CFUNC(mvtools_pixel_ssd_8x16_mmx);
-MK_CFUNC(mvtools_pixel_ssd_8x8_mmx);
-MK_CFUNC(mvtools_pixel_ssd_8x4_mmx);
-MK_CFUNC(mvtools_pixel_ssd_4x8_mmx);
 MK_CFUNC(mvtools_pixel_ssd_4x4_mmx);
+MK_CFUNC(mvtools_pixel_ssd_4x8_mmx);
+MK_CFUNC(mvtools_pixel_ssd_8x4_mmx);
+MK_CFUNC(mvtools_pixel_ssd_8x8_mmx);
+MK_CFUNC(mvtools_pixel_ssd_8x16_mmx);
+MK_CFUNC(mvtools_pixel_ssd_16x8_mmx);
+MK_CFUNC(mvtools_pixel_ssd_16x16_mmx);
 
 /* SATD: Sum of Absolute Transformed Differences, more sensitive to noise, frequency domain based - replacement to dct/SAD */
-MK_CFUNC(mvtools_pixel_satd_16x16_mmx2);
-MK_CFUNC(mvtools_pixel_satd_16x8_mmx2);
-MK_CFUNC(mvtools_pixel_satd_8x16_mmx2);
-MK_CFUNC(mvtools_pixel_satd_8x8_mmx2);
-MK_CFUNC(mvtools_pixel_satd_8x4_mmx2);
-MK_CFUNC(mvtools_pixel_satd_4x8_mmx2);
 MK_CFUNC(mvtools_pixel_satd_4x4_mmx2);
 
-#define SATD_SSE2(blsizex, blsizey) extern "C" unsigned int  mvtools_pixel_satd_##blsizex##x##blsizey##_sse2(const uint8_t *pSrc, intptr_t nSrcPitch, const uint8_t *pRef, intptr_t nRefPitch)
-#define SATD_SSSE3(blsizex, blsizey) extern "C" unsigned int  mvtools_pixel_satd_##blsizex##x##blsizey##_ssse3(const uint8_t *pSrc, intptr_t nSrcPitch, const uint8_t *pRef, intptr_t nRefPitch)
+MK_CFUNC(mvtools_pixel_satd_8x4_sse2);
+MK_CFUNC(mvtools_pixel_satd_8x8_sse2);
+MK_CFUNC(mvtools_pixel_satd_16x8_sse2);
+MK_CFUNC(mvtools_pixel_satd_16x16_sse2);
 
-//mvtools_pixel_satd_16x16_%1
-SATD_SSE2(16, 16);
-SATD_SSE2(16,  8);
-SATD_SSE2( 8, 16);
-SATD_SSE2( 8,  8);
-SATD_SSE2( 8,  4);
-SATD_SSSE3(16, 16);
-SATD_SSSE3(16,  8);
-SATD_SSSE3( 8, 16);
-SATD_SSSE3( 8,  8);
-SATD_SSSE3( 8,  4);
-#undef SATD_SSE2
-#undef SATD_SSSE3
+MK_CFUNC(mvtools_pixel_satd_8x4_ssse3);
+MK_CFUNC(mvtools_pixel_satd_8x8_ssse3);
+MK_CFUNC(mvtools_pixel_satd_8x16_ssse3);
+MK_CFUNC(mvtools_pixel_satd_16x8_ssse3);
+MK_CFUNC(mvtools_pixel_satd_16x16_ssse3);
 
 //dummy for testing and deactivate SAD
 MK_CFUNC(SadDummy);
