dubhater/vapoursynth-mvtools

commit d40773cf928f0bef95f43ee28c35c471f7ac43bf

Add support for 16 bits per sample
Only Mask left.

src/asm/Overlap.asm
@@ -37,8 +37,7 @@ cglobal Overlaps2x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS2
@@ -54,8 +53,7 @@ cglobal Overlaps2x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS2
@@ -94,8 +92,7 @@ cglobal Overlaps4x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS4
@@ -111,8 +108,7 @@ cglobal Overlaps4x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS4
@@ -130,8 +126,7 @@ cglobal Overlaps4x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS4
@@ -203,8 +198,7 @@ cglobal Overlaps8x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS8
@@ -220,8 +214,7 @@ cglobal Overlaps8x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS8
@@ -239,8 +232,7 @@ cglobal Overlaps8x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS8
@@ -263,8 +255,7 @@ cglobal Overlaps8x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS8
@@ -308,8 +299,7 @@ cglobal Overlaps16x1_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -324,8 +314,7 @@ cglobal Overlaps16x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -341,8 +330,7 @@ cglobal Overlaps16x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -360,8 +348,7 @@ cglobal Overlaps16x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -384,8 +371,7 @@ cglobal Overlaps16x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, w
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -418,8 +404,7 @@ cglobal Overlaps16x32_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, w
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS16
@@ -485,8 +470,7 @@ cglobal Overlaps32x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, wi
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS32
@@ -509,8 +493,7 @@ cglobal Overlaps32x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, w
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS32
@@ -543,8 +526,7 @@ cglobal Overlaps32x32_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, w
     movdqa m6, [dword256]
     pxor m7, m7
 
-    ; They're in pixels, apparently.
-    add dst_strideq, dst_strideq
+    ; It's in pixels, apparently.
     add win_strideq, win_strideq
 
     OVERS32

Makefile.am
@@ -21,7 +21,6 @@ libmvtools_la_SOURCES = src/asm-placeholder.cpp \
 						src/FakeGroupOfPlanes.cpp \
 						src/FakePlaneOfBlocks.cpp \
 						src/GroupOfPlanes.cpp \
-						src/Interpolation.cpp \
 						src/MaskFun.cpp \
 						src/MVAnalyse.cpp \
 						src/MVBlockFPS.cpp \
@@ -39,7 +38,6 @@ libmvtools_la_SOURCES = src/asm-placeholder.cpp \
 						src/MVSCDetection.cpp \
 						src/MVSuper.cpp \
 						src/Overlap.cpp \
-						src/Padding.cpp \
 						src/PlaneOfBlocks.cpp \
 						src/SimpleResize.cpp \
 						src/asm/const-a.asm \

src/CopyCode.h
@@ -7,12 +7,12 @@
 typedef void (*COPYFunction)(unsigned char *pDst, intptr_t nDstPitch,
         const unsigned char *pSrc, intptr_t nSrcPitch);
 
-template<int nBlkWidth, int nBlkHeight>
+template<int nBlkWidth, int nBlkHeight, typename PixelType>
 void Copy_C(uint8_t *pDst, intptr_t nDstPitch, const uint8_t *pSrc, intptr_t nSrcPitch)
 {
     for ( int j = 0; j < nBlkHeight; j++ )
     {
-        memcpy(pDst, pSrc, nBlkWidth);
+        memcpy(pDst, pSrc, nBlkWidth * sizeof(PixelType));
         pDst += nDstPitch;
         pSrc += nSrcPitch;
     }

src/DCT.cpp
@@ -1,9 +1,10 @@
 #include "DCT.h"
 
-DCTClass::DCTClass(int _sizex, int _sizey, int _dctmode):
+DCTClass::DCTClass(int _sizex, int _sizey, int _dctmode, int _bitsPerSample):
       sizex(_sizex)
     , sizey(_sizey)
     , dctmode(_dctmode)
+    , bitsPerSample(_bitsPerSample)
 {
 }
 

src/DCT.h
@@ -25,7 +25,7 @@ class DCTClass {
 
     public:
 
-        DCTClass(int _sizex, int _sizey, int _dctmode);
+        DCTClass(int _sizex, int _sizey, int _dctmode, int _bitsPerSample);
         virtual ~DCTClass();
 
         virtual void DCTBytes2D(const unsigned char *srcp0, int _src_pitch,
@@ -34,6 +34,7 @@ class DCTClass {
         int sizex;
         int sizey;
         int dctmode;
+        int bitsPerSample;
 };
 
 #endif

src/DCTFFTW.cpp
@@ -26,8 +26,8 @@
 std::mutex g_fftw_plans_mutex;
 
 
-DCTFFTW::DCTFFTW(int _sizex, int _sizey, int _dctmode) :
-    DCTClass(_sizex , _sizey, _dctmode)
+DCTFFTW::DCTFFTW(int _sizex, int _sizey, int _dctmode, int _bitsPerSample) :
+    DCTClass(_sizex , _sizey, _dctmode, _bitsPerSample)
 {
     int size2d = sizey*sizex;
 
@@ -60,22 +60,32 @@ DCTFFTW::~DCTFFTW()
 }
 
 //  put source data to real array for FFT
-void DCTFFTW::Bytes2Float (const unsigned char * srcp, int src_pitch, float * realdata)
+template <typename PixelType>
+void DCTFFTW::Bytes2Float (const unsigned char * srcp8, int src_pitch, float * realdata)
 {
     int floatpitch = sizex;
     int i, j;
     for (j = 0; j < sizey; j++) {
         for (i = 0; i < sizex; i+=1) {
+            PixelType *srcp = (PixelType *)srcp8;
             realdata[i] = srcp[i];
         }
-        srcp += src_pitch;
+        srcp8 += src_pitch;
         realdata += floatpitch;
     }
 }
 
 //  put source data to real array for FFT
-void DCTFFTW::Float2Bytes (unsigned char * dstp, int dst_pitch, float * realdata)
+template <typename PixelType>
+void DCTFFTW::Float2Bytes (unsigned char * dstp8, int dst_pitch, float * realdata)
 {
+    PixelType *dstp = (PixelType *)dstp8;
+
+    dst_pitch /= sizeof(PixelType);
+
+    int pixelMax = (1 << bitsPerSample) - 1;
+    int pixelHalf = 1 << (bitsPerSample - 1);
+
     int floatpitch = sizex;
     int i, j;
     int integ;
@@ -86,7 +96,7 @@ void DCTFFTW::Float2Bytes (unsigned char * dstp, int dst_pitch, float * realdata
        */
     // XXX function call to nearbyintf can be avoided by using cvtss2si
     integ = (int)(nearbyintf(f));
-    dstp[0] = std::min(255, std::max(0, (integ>>dctshift0) + 128)); // DC
+    dstp[0] = std::min(pixelMax, std::max(0, (integ>>dctshift0) + pixelHalf)); // DC
     for (i = 1; i < sizex; i+=1) {
         f = realdata[i]*0.707f; // to be compatible with integer DCTINT8
         /*
@@ -94,7 +104,7 @@ void DCTFFTW::Float2Bytes (unsigned char * dstp, int dst_pitch, float * realdata
            _asm fistp integ;
            */
         integ = (int)(nearbyintf(f));
-        dstp[i] = std::min(255, std::max(0, (integ>>dctshift) + 128));
+        dstp[i] = std::min(pixelMax, std::max(0, (integ>>dctshift) + pixelHalf));
     }
     dstp += dst_pitch;
     realdata += floatpitch;
@@ -106,7 +116,7 @@ void DCTFFTW::Float2Bytes (unsigned char * dstp, int dst_pitch, float * realdata
                _asm fistp integ;
                */
             integ = (int)(nearbyintf(f));
-            dstp[i] = std::min(255, std::max(0, (integ>>dctshift) + 128));
+            dstp[i] = std::min(pixelMax, std::max(0, (integ>>dctshift) + pixelHalf));
         }
         dstp += dst_pitch;
         realdata += floatpitch;
@@ -116,8 +126,13 @@ void DCTFFTW::Float2Bytes (unsigned char * dstp, int dst_pitch, float * realdata
 
 void DCTFFTW::DCTBytes2D(const unsigned char *srcp, int src_pitch, unsigned char *dctp, int dct_pitch)
 {
-    Bytes2Float (srcp, src_pitch, fSrc);
-    fftwf_execute_r2r(dctplan, fSrc, fSrcDCT);
-    Float2Bytes (dctp, dct_pitch, fSrcDCT);
-
+    if (bitsPerSample == 8) {
+        Bytes2Float<uint8_t>(srcp, src_pitch, fSrc);
+        fftwf_execute_r2r(dctplan, fSrc, fSrcDCT);
+        Float2Bytes<uint8_t>(dctp, dct_pitch, fSrcDCT);
+    } else {
+        Bytes2Float<uint16_t>(srcp, src_pitch, fSrc);
+        fftwf_execute_r2r(dctplan, fSrc, fSrcDCT);
+        Float2Bytes<uint16_t>(dctp, dct_pitch, fSrcDCT);
+    }
 }

src/DCTFFTW.h
@@ -32,14 +32,17 @@ class DCTFFTW : public DCTClass {
     int dctshift;
     int dctshift0;
 
+    template <typename PixelType>
     void Bytes2Float(const unsigned char * srcp0, int _pitch, float * realdata);
+
+    template <typename PixelType>
     void Float2Bytes(unsigned char * srcp0, int _pitch, float * realdata);
 
     public:
 
 
 
-    DCTFFTW(int _sizex, int _sizey, int _dctmode);
+    DCTFFTW(int _sizex, int _sizey, int _dctmode, int _bitsPerSample);
     ~DCTFFTW();
 
     virtual void DCTBytes2D(const unsigned char *srcp0, int _src_pitch,

src/GroupOfPlanes.cpp
@@ -19,7 +19,7 @@
 
 #include "GroupOfPlanes.h"
 
-GroupOfPlanes::GroupOfPlanes(int _nBlkSizeX, int _nBlkSizeY, int _nLevelCount, int _nPel, int _nFlags, int _nOverlapX, int _nOverlapY, int _nBlkX, int _nBlkY, int _xRatioUV, int _yRatioUV, int _divideExtra)
+GroupOfPlanes::GroupOfPlanes(int _nBlkSizeX, int _nBlkSizeY, int _nLevelCount, int _nPel, int _nFlags, int _nOverlapX, int _nOverlapY, int _nBlkX, int _nBlkY, int _xRatioUV, int _yRatioUV, int _divideExtra, int _bitsPerSample)
 {
     nBlkSizeX = _nBlkSizeX;
     nBlkSizeY = _nBlkSizeY;
@@ -31,6 +31,7 @@ GroupOfPlanes::GroupOfPlanes(int _nBlkSizeX, int _nBlkSizeY, int _nLevelCount, i
     xRatioUV = _xRatioUV;
     yRatioUV = _yRatioUV;
     divideExtra = _divideExtra;
+    bitsPerSample = _bitsPerSample;
 
     planes = new PlaneOfBlocks*[nLevelCount];
 
@@ -49,7 +50,7 @@ GroupOfPlanes::GroupOfPlanes(int _nBlkSizeX, int _nBlkSizeY, int _nLevelCount, i
             nFlagsCurrent |= MOTION_SMALLEST_PLANE;
         nBlkX = ((nWidth_B>>i) - nOverlapX)/(nBlkSizeX-nOverlapX);
         nBlkY = ((nHeight_B>>i) - nOverlapY)/(nBlkSizeY-nOverlapY);
-        planes[i] = new PlaneOfBlocks(nBlkX, nBlkY, nBlkSizeX, nBlkSizeY, nPelCurrent, i, nFlagsCurrent, nOverlapX, nOverlapY, xRatioUV, yRatioUV);
+        planes[i] = new PlaneOfBlocks(nBlkX, nBlkY, nBlkSizeX, nBlkSizeY, nPelCurrent, i, nFlagsCurrent, nOverlapX, nOverlapY, xRatioUV, yRatioUV, bitsPerSample);
         nPelCurrent = 1;
     }
 }

commit 506d211edbe4dd48f69f87254c068f079ec9c00e

Fix pink tint with the ASM overlap functions
Bug introduced in d40773cf928f0bef95f43ee28c35c471f7ac43bf, where I
removed an addition from the C version, but not from the ASM versions.

Fixes #13.

src/asm/Overlap.asm
@@ -1,11 +1,6 @@
 %include "include/x86inc.asm"
 
 
-SECTION_RODATA
-
-dword256 times 4 dd 0x00000100
-
-
 SECTION_TEXT
 
 
@@ -17,7 +12,6 @@ SECTION_TEXT
     pmullw m0, m1
     pmulhw m2, m1
     punpcklwd m0, m2
-    paddd m0, m6
     psrld m0, 6
     packssdw m0, m7
     movd m1, [dstpq]
@@ -34,7 +28,6 @@ INIT_XMM
 cglobal Overlaps2x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
     ; It's in pixels, apparently.
@@ -50,7 +43,6 @@ INIT_XMM
 cglobal Overlaps2x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
     ; It's in pixels, apparently.
@@ -72,7 +64,6 @@ cglobal Overlaps2x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     pmullw m0, m1
     pmulhw m2, m1
     punpcklwd m0, m2
-    paddd m0, m6
     psrld m0, 6
     packssdw m0, m7
     movq m1, [dstpq]
@@ -89,7 +80,6 @@ INIT_XMM
 cglobal Overlaps4x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
     ; It's in pixels, apparently.
@@ -105,7 +95,6 @@ INIT_XMM
 cglobal Overlaps4x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
     ; It's in pixels, apparently.
@@ -123,7 +112,6 @@ INIT_XMM
 cglobal Overlaps4x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7    ; =0
 
     ; It's in pixels, apparently.
@@ -161,8 +149,6 @@ cglobal Overlaps4x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win
     movdqa m1, m0
     punpcklwd m0, m2
     punpckhwd m1, m2
-    paddd m0, m6
-    paddd m1, m6
     psrld m0, 6
     psrld m1, 6
     packssdw m0, m1
@@ -183,7 +169,6 @@ INIT_XMM
 cglobal Overlaps8x1_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     OVERS8
@@ -195,7 +180,6 @@ INIT_XMM
 cglobal Overlaps8x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -211,7 +195,6 @@ INIT_XMM
 cglobal Overlaps8x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -229,7 +212,6 @@ INIT_XMM
 cglobal Overlaps8x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -252,7 +234,6 @@ INIT_XMM
 cglobal Overlaps8x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -296,7 +277,6 @@ INIT_XMM
 cglobal Overlaps16x1_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -311,7 +291,6 @@ INIT_XMM
 cglobal Overlaps16x2_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -327,7 +306,6 @@ INIT_XMM
 cglobal Overlaps16x4_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -345,7 +323,6 @@ INIT_XMM
 cglobal Overlaps16x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -368,7 +345,6 @@ INIT_XMM
 cglobal Overlaps16x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -401,7 +377,6 @@ INIT_XMM
 cglobal Overlaps16x32_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -467,7 +442,6 @@ INIT_XMM
 cglobal Overlaps32x8_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -490,7 +464,6 @@ INIT_XMM
 cglobal Overlaps32x16_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.
@@ -523,7 +496,6 @@ INIT_XMM
 cglobal Overlaps32x32_sse2, 6, 6, 8, dstp, dst_stride, srcp, src_stride, winp, win_stride
 
     ; prepare constants
-    movdqa m6, [dword256]
     pxor m7, m7
 
     ; It's in pixels, apparently.