433cef1ca4e091262f3b0b042146301959d0f00a
cantabile <cantabile.desu@gmail.com>
2015-09-04 10:43:37 +0300

    BlockFPS: fix crash with gray input

--- src/MVBlockFPS.cpp
+++ src/MVBlockFPS.cpp
@@ -692,7 +692,8 @@ static void VS_CC mvblockfpsFree(void *instanceData, VSCore *core, const VSAPI *
     delete d->bleh;
 
     delete d->upsizer;
-    delete d->upsizerUV;
+    if (d->nSuperModeYUV & UVPLANES)
+        delete d->upsizerUV;
 
     delete [] d->OnesBlock;
 
