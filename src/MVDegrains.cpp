dubhater/vapoursynth-mvtools

commit 691a7a99b7396f871ab3c6f9583becf890c6545b

Convert almost everything to C
Mostly because I can.

Unfortunately some unrelated changes snuck in, such as the removal of
MVClip*, MVFilter, etc.

src/MVDegrains.h
@@ -6,6 +6,7 @@
 
 #include <emmintrin.h>
 
+#include "Fakery.h"
 #include "MVFrame.h"
 
 enum VectorOrder {
@@ -189,14 +190,14 @@ inline int DegrainWeight(int64_t thSAD, int64_t blockSAD) {
 }
 
 
-inline void useBlock(const uint8_t *&p, int &np, int &WRef, bool isUsable, const MVClipBalls *mvclip, int i, const MVPlane *pPlane, const uint8_t **pSrcCur, int xx, const int *nSrcPitch, int nLogPel, int plane, int xSubUV, int ySubUV, const int *thSAD) {
+inline void useBlock(const uint8_t *&p, int &np, int &WRef, bool isUsable, const FakeGroupOfPlanes *fgop, int i, const MVPlane *pPlane, const uint8_t **pSrcCur, int xx, const int *nSrcPitch, int nLogPel, int plane, int xSubUV, int ySubUV, const int *thSAD) {
     if (isUsable) {
-        const FakeBlockData &block = mvclip->GetBlock(0, i);
-        int blx = (block.GetX() << nLogPel) + block.GetMV().x;
-        int bly = (block.GetY() << nLogPel) + block.GetMV().y;
-        p = pPlane->GetPointer(plane ? blx >> xSubUV : blx, plane ? bly >> ySubUV : bly);
-        np = pPlane->GetPitch();
-        int blockSAD = block.GetSAD();
+        const FakeBlockData *block = fgopGetBlock(fgop, 0, i);
+        int blx = (block->x << nLogPel) + block->vector.x;
+        int bly = (block->y << nLogPel) + block->vector.y;
+        p = mvpGetPointer(pPlane, plane ? blx >> xSubUV : blx, plane ? bly >> ySubUV : bly);
+        np = pPlane->nPitch;
+        int blockSAD = block->vector.sad;
         WRef = DegrainWeight(thSAD[plane], blockSAD);
     } else {
         p = pSrcCur[plane] + xx;

commit 21067bc85aeaa834d2acc43ab24d589d43aa02ea

Degrains: fix crash due to bad logic
Bug introduced in 691a7a99b7396f871ab3c6f9583becf890c6545b

src/MVDegrains.cpp
@@ -255,7 +255,7 @@ static const VSFrameRef *VS_CC mvdegrainGetFrame(int n, int activationReason, vo
                         int WSrc, WRefs[radius * 2];
 
                         for (int r = 0; r < radius * 2; r++)
-                            useBlock(pointers[r], strides[r], WRefs[r], isUsable[r], &fgops[r], i, pPlanes[r][plane], pSrcCur, xx, nSrcPitches, nLogPel, plane, xSubUV, ySubUV, thSAD);
+                            useBlock(pointers[r], strides[r], WRefs[r], isUsable[r], &fgops[r], i, pPlanes[r], pSrcCur, xx, nSrcPitches, nLogPel, plane, xSubUV, ySubUV, thSAD);
 
                         normaliseWeights<radius>(WSrc, WRefs);
 
@@ -298,7 +298,7 @@ static const VSFrameRef *VS_CC mvdegrainGetFrame(int n, int activationReason, vo
                         int WSrc, WRefs[radius * 2];
 
                         for (int r = 0; r < radius * 2; r++)
-                            useBlock(pointers[r], strides[r], WRefs[r], isUsable[r], &fgops[r], i, pPlanes[r][plane], pSrcCur, xx, nSrcPitches, nLogPel, plane, xSubUV, ySubUV, thSAD);
+                            useBlock(pointers[r], strides[r], WRefs[r], isUsable[r], &fgops[r], i, pPlanes[r], pSrcCur, xx, nSrcPitches, nLogPel, plane, xSubUV, ySubUV, thSAD);
 
                         normaliseWeights<radius>(WSrc, WRefs);
 

src/MVDegrains.h
@@ -190,13 +190,13 @@ inline int DegrainWeight(int64_t thSAD, int64_t blockSAD) {
 }
 
 
-inline void useBlock(const uint8_t *&p, int &np, int &WRef, bool isUsable, const FakeGroupOfPlanes *fgop, int i, const MVPlane *pPlane, const uint8_t **pSrcCur, int xx, const int *nSrcPitch, int nLogPel, int plane, int xSubUV, int ySubUV, const int *thSAD) {
+inline void useBlock(const uint8_t *&p, int &np, int &WRef, bool isUsable, const FakeGroupOfPlanes *fgop, int i, MVPlane * const *pPlane, const uint8_t **pSrcCur, int xx, const int *nSrcPitch, int nLogPel, int plane, int xSubUV, int ySubUV, const int *thSAD) {
     if (isUsable) {
         const FakeBlockData *block = fgopGetBlock(fgop, 0, i);
         int blx = (block->x << nLogPel) + block->vector.x;
         int bly = (block->y << nLogPel) + block->vector.y;
-        p = mvpGetPointer(pPlane, plane ? blx >> xSubUV : blx, plane ? bly >> ySubUV : bly);
-        np = pPlane->nPitch;
+        p = mvpGetPointer(pPlane[plane], plane ? blx >> xSubUV : blx, plane ? bly >> ySubUV : bly);
+        np = pPlane[plane]->nPitch;
         int blockSAD = block->vector.sad;
         WRef = DegrainWeight(thSAD[plane], blockSAD);
     } else {