bc1733e2fa0fe208e6a3baa8018c18a2eb6a00d4
HolyWu <holywu@gmail.com>
2016-06-04 12:14:08 +0800

    Only define snprintf if compiling with MSVC 2013 and below

--- src/Bullshit.h
+++ src/Bullshit.h
@@ -1,7 +1,7 @@
 #ifndef BULLSHIT_H
 #define BULLSHIT_H
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && _MSC_VER < 1900
 // Don't forget to zero the last byte. _snprintf doesn't do it if the string doesn't fit.
 #define snprintf _snprintf
 #endif
