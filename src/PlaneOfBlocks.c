eb6b5d17a86e0ed2359f590593e0a73348838941
cantabile <cantabile.desu@gmail.com>
2016-09-05 19:59:03 +0300

    Silence warning from GCC

--- src/PlaneOfBlocks.c
+++ src/PlaneOfBlocks.c
@@ -17,6 +17,8 @@
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
 // http://www.gnu.org/copyleft/gpl.html .
 
+// For posix_memalign.
+#define _POSIX_C_SOURCE 200112L
 #include <VSHelper.h>
 
 #include "CPU.h"
